845d5621455b598b72b624ef577d01cb2bd63939
Mitch Capper <mitch.capper@gmail.com>
2017-05-12 18:25:57 -0500

    Expose 3 additional interfaces under the Interfaces namespace Allow users to declare these items.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,22 @@
-import * as api from "./api";
-export {connect} from "./connect";
+﻿ import * as api from "./api";
+export { connect } from "./connect";
+import { ConnectOptions as ConOpts } from "./connect";
 import * as apiInterface from "./interfaces/api/api";
 import * as contact from "./interfaces/api/contact";
 import * as conversation from "./interfaces/api/conversation";
+import * as events from "./interfaces/api/events";
+import * as resources from "./interfaces/api/resources";
 
 export type Api = api.Api;
 export namespace Api {
   export type NewMessage = apiInterface.NewMessage;
   export type SendMessageResult = apiInterface.SendMessageResult;
 }
-
+export namespace Interfaces {
+  export type EventMessage = events.EventMessage;
+  export type ConnectOptions = ConOpts;
+  export type TextResource = resources.TextResource;
+}
 export type Contact = contact.Contact;
 export namespace Contact {
   export type Phone = contact.Phone;
