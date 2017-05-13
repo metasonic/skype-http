085454c3e473d9a524bd2df0987d8781e751ae0e
Mitch Capper <mitch.capper@gmail.com>
2017-05-13 11:17:51 -0500

    more re-org for namespaces

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,6 +7,18 @@ import * as events from "./interfaces/api/events";
 export { events };
 import * as resources from "./interfaces/api/resources";
 export { resources };
+import * as nativeContact from "./interfaces/native-api/contact";
+import * as nativeConversation from "./interfaces/native-api/conversation";
+import * as nativeEvents from "./interfaces/native-api/events";
+import * as nativeMessageResources from "./interfaces/native-api/message-resources";
+import * as nativeResources from "./interfaces/native-api/resources";
+namespace native {
+  export import contact = nativeContact;
+  export import conversation = nativeConversation;
+  export import resources = nativeResources;
+  export import events = nativeEvents;
+  export import messageResources = nativeMessageResources;
+}
 export type Api = api.Api;
 export namespace Api {
   export type NewMessage = apiInterface.NewMessage;
@@ -14,12 +26,14 @@ export namespace Api {
 }
 export type Contact = contact.Contact;
 export namespace Contact {
+  export type Contact = contact.Contact;
   export type Phone = contact.Phone;
   export type Location = contact.Location;
 }
 
 export type Conversation = conversation.Conversation;
 export namespace Conversation {
+  export type Conversation = conversation.Conversation;
   export type ThreadProperties = conversation.ThreadProperties;
 }
 
