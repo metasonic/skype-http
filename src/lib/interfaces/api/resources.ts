072827827a7b45c5acb2a9bfe2dd8083737f75d7
Mitch Capper <mitch.capper@gmail.com>
2017-05-17 15:32:27 -0700

    Added more resource / message types. Refactored message-poller code. Some minor BOM fixes.

--- src/lib/interfaces/api/resources.ts
+++ src/lib/interfaces/api/resources.ts
@@ -1,7 +1,14 @@
-import {ParsedConversationId} from "./api";
+import { ParsedConversationId } from "./api";
+export interface CallParticipant {
+  duration?: number;
+  displayName: string;
+  username: string;
+}
 
+export declare type ResourceType = "Text" | "RichText" | "Control/ClearTyping" | "Control/Typing" | "RichText/UriObject"
+  | "RichText/Media_GenericFile" | "Signal/Flamingo" | "Event/Call" | "RichText/Location" | "ConversationUpdate";
 export interface Resource {
-  type: "Text" | "RichText" | "Control/ClearTyping" | "Control/Typing" | "ConversationUpdate" /* | "Typing" | ... */;
+  type: ResourceType;
   id: string;
   composeTime: Date;
   arrivalTime: Date;
@@ -15,11 +22,48 @@ export interface TextResource extends Resource {
   clientId: string; // An id set by the client
   content: string;
 }
+export interface RichTextLocationResource extends Resource {
+  latitude: number;
+  longitude: number;
+  altitude: number;
+  speed: number;
+  course: number;
+  address: string;
+  pointOfInterest: string;
+  map_url: string;
+}
+export interface RichTextMediaGenericFileResource extends FileResource {
+  type: "RichText/Media_GenericFile";
+}
 
+export interface RichTextUriObjectResource extends FileResource {
+  type: "RichText/UriObject";
+}
+export interface FileResource extends Resource {
+  uri_type: string;
+  uri: string;
+  uri_thumbnail: string;
+  uri_w_login: string;
+  file_size?: number;
+  original_file_name: string;
+}
+export interface EventCallResource extends Resource {
+  event_type: "started" | "ended";
+  duration?: number; // duration of the shorted participant on the call
+  call_connected: boolean; // if it was connected or missed
+  skypeguid: string;
+  participants: CallParticipant[];
+}
 export interface RichTextResource extends Resource {
   type: "RichText";
   clientId: string; // An id set by the client
   content: string;
+}
+
+export interface SignalFlamingoResource extends Resource { // incoming call request
+  type: "Signal/Flamingo";
+  skypeguid: string;
+
 }
 
 export interface ControlClearTypingResource extends Resource {
